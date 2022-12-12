54c905b27844d0585fb2ca86376f39cfd8cd5843
Gip886 <798484107@qq.com>
2022-12-12 23:03:07 +0800

    feat: tabs

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import styles from './styles.module.scss'
 
-const Tabs:React.FC = () => {
+const Tabs: React.FC = () => {
   return (
-   <div className={styles.tabs} >
-        Tabs
-   </div>
+    <div className={styles.tabs}>
+      <li>大会员</li>
+      <li>消息</li>
+      <li>动态</li>
+    </div>
   )
 }
 
